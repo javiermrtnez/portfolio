6510fc8a50c9af5030745841abc0e080b30174b2
javiermrtnez <javiermartinezbueno@gmail.com>
2023-06-07 19:41:13 +0200

    feat: add Inter var font

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Inter var', 'system-ui', 'Avenir', 'Helvetica', 'Arial', 'sans-serif'],
+      },
       animation: {
         'hi': 'hi 1.5s linear -.5s infinite',
       },
