482bdff29590b8a0380d9d1615637e68dd342757
javiermrtnez <javiermartinezbueno@gmail.com>
2023-06-08 12:40:29 +0200

    style: technology less opacity and quicker transaction

--- src/components/Technology.tsx
+++ src/components/Technology.tsx
@@ -8,7 +8,7 @@ const Technology = ({ name, icon, href }: Props) => {
   return (
     <a
       href={href}
-      className='w-32 h-32 flex flex-col gap-3 items-center justify-center border border-neutral-700 rounded-lg saturate-0 opacity-40 hover:saturate-100 hover:scale-[1.01] hover:opacity-100 transition-all duration-300'
+      className='w-32 h-32 flex flex-col gap-3 items-center justify-center border border-neutral-700 rounded-lg saturate-0 opacity-50 hover:saturate-100 hover:scale-[1.01] hover:opacity-100 transition-all duration-200'
     >
       <div className='w-12 h-12'>{icon}</div>
       <h2 className='font-semibold text-sm'>{name}</h2>
