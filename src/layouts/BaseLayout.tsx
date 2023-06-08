27af1e2721f151e230e89c84e2876ca31e270613
javiermrtnez <javiermartinezbueno@gmail.com>
2023-06-08 18:51:11 +0200

    feat: add padding bottom

--- src/layouts/BaseLayout.tsx
+++ src/layouts/BaseLayout.tsx
@@ -4,7 +4,7 @@ interface Props {
 
 const BaseLayout = ({ children }: Props) => {
   return (
-    <div className='flex flex-col gap-20 w-full mx-auto max-w-[var(--page-width-with-padding)] px-[var(--page-padding)]'>
+    <div className='flex flex-col gap-20 w-full mx-auto max-w-[var(--page-width-with-padding)] px-[var(--page-padding)] pb-10'>
       {children}
     </div>
   );
