9e08149f8f74fba4871d4e2b3d89db8b9248811a
javiermrtnez <javiermartinezbueno@gmail.com>
2023-06-08 17:28:10 +0200

    fix: replace cursor pointer by default

--- src/components/Technology.tsx
+++ src/components/Technology.tsx
@@ -5,7 +5,7 @@ interface Props {
 
 const Technology = ({ name, icon }: Props) => {
   return (
-    <div className='w-32 h-32 flex flex-col gap-3 items-center justify-center cursor-pointer border border-neutral-700 rounded-lg saturate-0 opacity-50 hover:saturate-100 hover:scale-[1.01] hover:opacity-100 transition-all duration-200'>
+    <div className='w-32 h-32 flex flex-col gap-3 items-center justify-center cursor-default border border-neutral-700 rounded-lg saturate-0 opacity-50 hover:saturate-100 hover:scale-[1.01] hover:opacity-100 transition-all duration-200'>
       <div className='w-12 h-12'>{icon}</div>
       <h2 className='font-semibold text-sm'>{name}</h2>
     </div>
