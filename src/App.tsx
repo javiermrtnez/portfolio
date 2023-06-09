d3a909e48cf32fef66a324f354524b1d7b0b434c
javiermrtnez <javiermartinezbueno@gmail.com>
2023-06-09 21:21:55 +0200

    refactor: create array of sections

--- src/App.tsx
+++ src/App.tsx
@@ -8,55 +8,72 @@ import BaseLayout from './layouts/BaseLayout';
 import Experience from './components/Experience';
 
 function App() {
+  const SECTIONS = [
+    {
+      id: SECTIONS_IDS.HOME,
+      component: <Home />,
+    },
+    {
+      id: SECTIONS_IDS.ABOUT_ME,
+      title: SECTIONS_NAMES.ABOUT_ME,
+      component: <AboutMe />,
+    },
+    {
+      id: SECTIONS_IDS.TECHNOLOGIES,
+      title: SECTIONS_NAMES.TECHNOLOGIES,
+      component: <Technologies />,
+    },
+    {
+      id: SECTIONS_IDS.EXPERIENCE,
+      title: SECTIONS_NAMES.EXPERIENCE,
+      component: <Experience />,
+    },
+    {
+      id: SECTIONS_IDS.PROJECTS,
+      title: SECTIONS_NAMES.PROJECTS,
+      component: (
+        <div className='flex flex-col gap-4 text-[14.5px] text-neutral-400'>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas felis orci, congue sed
+            turpis ut, laoreet aliquam mi. Cras vitae congue nunc, sit amet tempus dolor. Donec
+            fermentum turpis ut mauris iaculis, vitae malesuada dui commodo. Nullam id massa sed
+            nunc malesuada congue vel vitae nisl. Curabitur iaculis, leo fringilla dignissim
+            aliquet, dui quam eleifend eros, eget tincidunt lorem lacus at nulla. Mauris non
+            elementum erat. Aenean leo enim, pharetra vitae ipsum vitae, tristique pellentesque leo.
+            Vivamus ut pharetra purus.
+          </p>
+        </div>
+      ),
+    },
+    {
+      id: SECTIONS_IDS.EDUCATION,
+      title: SECTIONS_NAMES.EDUCATION,
+      component: (
+        <div className='flex flex-col gap-4 text-[14.5px] text-neutral-400'>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas felis orci, congue sed
+            turpis ut, laoreet aliquam mi. Cras vitae congue nunc, sit amet tempus dolor. Donec
+            fermentum turpis ut mauris iaculis, vitae malesuada dui commodo. Nullam id massa sed
+            nunc malesuada congue vel vitae nisl. Curabitur iaculis, leo fringilla dignissim
+            aliquet, dui quam eleifend eros, eget tincidunt lorem lacus at nulla. Mauris non
+            elementum erat. Aenean leo enim, pharetra vitae ipsum vitae, tristique pellentesque leo.
+            Vivamus ut pharetra purus.
+          </p>
+        </div>
+      ),
+    },
+  ];
+
   return (
     <>
       <Navbar />
 
-      {/* TO-DO: Refactor */}
       <BaseLayout>
-        <Section id={SECTIONS_IDS.HOME}>
-          <Home />
-        </Section>
-
-        <Section id={SECTIONS_IDS.ABOUT_ME} title={SECTIONS_NAMES.ABOUT_ME}>
-          <AboutMe />
-        </Section>
-
-        <Section id={SECTIONS_IDS.TECHNOLOGIES} title={SECTIONS_NAMES.TECHNOLOGIES}>
-          <Technologies />
-        </Section>
-
-        <Section id={SECTIONS_IDS.EXPERIENCE} title={SECTIONS_NAMES.EXPERIENCE}>
-          <Experience />
-        </Section>
-
-        <Section id={SECTIONS_IDS.PROJECTS} title={SECTIONS_NAMES.PROJECTS}>
-          <div className='flex flex-col gap-4 text-[14.5px] text-neutral-400'>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas felis orci, congue
-              sed turpis ut, laoreet aliquam mi. Cras vitae congue nunc, sit amet tempus dolor.
-              Donec fermentum turpis ut mauris iaculis, vitae malesuada dui commodo. Nullam id massa
-              sed nunc malesuada congue vel vitae nisl. Curabitur iaculis, leo fringilla dignissim
-              aliquet, dui quam eleifend eros, eget tincidunt lorem lacus at nulla. Mauris non
-              elementum erat. Aenean leo enim, pharetra vitae ipsum vitae, tristique pellentesque
-              leo. Vivamus ut pharetra purus.
-            </p>
-          </div>
-        </Section>
-
-        <Section id={SECTIONS_IDS.EDUCATION} title={SECTIONS_NAMES.EDUCATION}>
-          <div className='flex flex-col gap-4 text-[14.5px] text-neutral-400'>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas felis orci, congue
-              sed turpis ut, laoreet aliquam mi. Cras vitae congue nunc, sit amet tempus dolor.
-              Donec fermentum turpis ut mauris iaculis, vitae malesuada dui commodo. Nullam id massa
-              sed nunc malesuada congue vel vitae nisl. Curabitur iaculis, leo fringilla dignissim
-              aliquet, dui quam eleifend eros, eget tincidunt lorem lacus at nulla. Mauris non
-              elementum erat. Aenean leo enim, pharetra vitae ipsum vitae, tristique pellentesque
-              leo. Vivamus ut pharetra purus.
-            </p>
-          </div>
-        </Section>
+        {SECTIONS.map(({ id, title, component }) => (
+          <Section key={id} id={id} title={title}>
+            {component}
+          </Section>
+        ))}
       </BaseLayout>
     </>
   );
